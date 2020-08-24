54436eda44673de652457696f0af20ed0cf8c51c
T2D <tienduocict@gmail.com>
2020-08-24 22:49:36 +0700

    Fix get product issues

--- routes/products.js
+++ routes/products.js
@@ -4,19 +4,7 @@ const {database} = require('../config/heppers')
 
 /* GET ALL PRODUCTS */
 router.get('/', function (req, res) {
-    let page = (req.query.page !== undefined && req.query.page !== 0) ? req.query.page : 1; // set the current page number
-    const limit = (req.query.limit !== undefined && req.query.page !== 0) ? req.query.limit : 10; // set the limit of items per page
-
-    let startValue;
-    let endValue;
-
-    if (page > 0) {
-        startValue = (page * limit) - limit; // 0,10,20,30
-        endValue = page * limit;
-    } else {
-        startValue = 0;
-        endValue = 10;
-    }
+    let {startValue, endValue} = getStartEndValue(req);
 
     database.table('products as p')
         .join([{
@@ -46,7 +34,7 @@ router.get('/', function (req, res) {
 
 /* GET SINGLE PRODUCT */
 router.get('/:prodId', (req, res) => {
-    let productId = req.params.prodId;
+    let productId = req.params['prodId'];
     database.table('products as p')
         .join([{
             table: 'categories as c',
@@ -72,23 +60,10 @@ router.get('/:prodId', (req, res) => {
 
 /* GET ALL PRODUCTS FROM ONE PARTICULAR CATEGORY */
 router.get('/category/:catName', (req, res) => {
-
-    let page = (req.query.page !== undefined && req.query.page !== 0) ? req.query.page : 1; // set the current page number
-    const limit = (req.query.limit !== undefined && req.query.page !== 0) ? req.query.limit : 10; // set the limit of items per page
-
-    let startValue;
-    let endValue;
-
-    if (page > 0) {
-        startValue = (page * limit) - limit; // 0,10,20,30
-        endValue = page * limit;
-    } else {
-        startValue = 0;
-        endValue = 10;
-    }
+    let {startValue, endValue} = getStartEndValue(req);
 
     // Fetch the category name from the url
-    const cat_title = req.params.catName;
+    const cat_title = req.params['catName'];
 
     database.table('products as p')
         .join([{
@@ -115,5 +90,22 @@ router.get('/category/:catName', (req, res) => {
             }
         }).catch(err => console.log(err));
 });
+
+function getStartEndValue(req) {
+    let page = (req.query['page'] !== undefined && req.query['page'] !== 0) ? req.query['page'] : 1; // set the current page number
+    const limit = (req.query.limit !== undefined && req.query['page'] !== 0) ? req.query.limit : 10; // set the limit of items per page
+
+    let startValue;
+    let endValue;
+
+    if (page > 0) {
+        startValue = (page * limit) - limit; // 0,10,20,30
+        endValue = page * limit;
+    } else {
+        startValue = 0;
+        endValue = 10;
+    }
+    return {startValue, endValue};
+}
 
 module.exports = router;
